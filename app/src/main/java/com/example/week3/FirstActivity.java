372454dcbbef28ae573e2c81f43e7dbc85e514ad
Piyush Jangam <jangamp@algonquincollege.com>
2024-01-14 22:08:33 -0500

    Initial commit

---
+++ app/src/main/java/com/example/week3/FirstActivity.java
@@ -0,0 +1,46 @@
+package com.example.week3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // setContentView loads objects onto the screen.
+        // Before this function, the screen is empty.
+        setContentView(R.layout.activity_main);
+
+        Intent nextPage = new Intent(this, SecondActivity.class);
+        Button secondButton = findViewById(R.id.buttonToSecond);
+        secondButton.setOnClickListener( click -> startActivity( nextPage ));
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+}
