d888c134e719456009131617dd2bd4a2f174b314
Piyush Jangam <jangamp@algonquincollege.com>
2024-01-15 20:34:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/week6_asynctask/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.week6_asynctask;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyHTTPRequest req = new MyHTTPRequest();
+        req.execute("http://carapiet.com/files/CST2335_XML.xml");  //Type 1
+    }
+    //Type1     Type2   Type3
+    private class MyHTTPRequest extends AsyncTask< String, Integer, String>
+    {
+        //Type3                Type1
+        public String doInBackground(String ... args)
+        {
+            publishProgress(25);
+            publishProgress(50);
+            publishProgress(75);
+            return "Done";
+
+        }
+
+        //Type 2
+        public void onProgressUpdate(Integer ... args)
+        {
+
+        }
+        //Type3
+        public void onPostExecute(String fromDoInBackground)
+        {
+            Log.i("HTTP", fromDoInBackground);
+        }
+    }
+}
