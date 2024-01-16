db74a817527e7c155273d1a2b80e86e937165d82
Piyush Jangam <jangamp@algonquincollege.com>
2024-01-15 19:33:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/week4/FirstActivity.java
@@ -0,0 +1,21 @@
+package com.example.week4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // setContentView loads objects onto the screen.
+        // Before this function, the screen is empty.
+        setContentView(R.layout.activity_main);
+
+        ListView myList = findViewById(R.id.theListView);
+
+    }
+
+}
