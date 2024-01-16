98179782bc32644328ae9c57c63d75ba357e95b8
Piyush Jangam <jangamp@algonquincollege.com>
2024-01-15 19:45:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/week4_refresh_listener/FirstActivity.java
@@ -0,0 +1,92 @@
+package com.example.week4_refresh_listener;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FirstActivity extends AppCompatActivity {
+    private ArrayList<String> elements = new ArrayList<>( Arrays.asList( "one", "Two"/*Empty*/ ) );
+    private MyListAdapter myAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // setContentView loads objects onto the screen.
+        // Before this function, the screen is empty.
+        setContentView(R.layout.activity_main);
+
+        ListView myList = findViewById(R.id.theListView);
+        myList.setAdapter( myAdapter = new MyListAdapter());
+
+        myList.setOnItemLongClickListener( (p, b, pos, id) -> {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("A title")
+
+                    //What is the message:
+                    .setMessage("Do you want to add stuff")
+
+                    //what the Yes button does:
+                    .setPositiveButton("Yes", (click, arg) -> {
+                        elements.add("HELLO");
+                        myAdapter.notifyDataSetChanged();
+                    })
+                    //What the No button does:
+                    .setNegativeButton("No", (click, arg) -> { })
+
+                    //An optional third button:
+                    .setNeutralButton("Maybe", (click, arg) -> {  })
+
+                    //You can add extra layout elements:
+                    .setView(getLayoutInflater().inflate(R.layout.row_layout, null) )
+
+                    //Show the dialog
+                    .create().show();
+            return true;
+        });
+
+        //Whenever you swipe down on the list, do something:
+        SwipeRefreshLayout refresher = findViewById(R.id.refresher);
+        refresher.setOnRefreshListener( () -> refresher.setRefreshing(false)  );
+    }
+
+    private class MyListAdapter extends BaseAdapter{
+
+        public int getCount() { return elements.size(); }
+
+        public Object getItem(int position) { return "This is row " + position; }
+
+        public long getItemId(int position) { return (long) position; }
+
+        public View getView(int position, View old, ViewGroup parent)
+        {
+            View newView = old;
+            LayoutInflater inflater = getLayoutInflater();
+
+            //make a new row:
+            if(newView == null) {
+                newView = inflater.inflate(R.layout.row_layout, parent, false);
+
+            }
+            //set what the text should be for this row:
+            TextView tView = newView.findViewById(R.id.textGoesHere);
+            tView.setText( getItem(position).toString() );
+
+            //return it to be put in the table
+            return newView;
+        }
+    }
+}
