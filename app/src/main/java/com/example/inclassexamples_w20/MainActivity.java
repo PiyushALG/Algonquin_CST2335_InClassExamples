ca2f5934a0eb042da3f8e9d2b10298ec06c87925
Piyush Jangam <jangamp@algonquincollege.com>
2024-01-16 20:18:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/inclassexamples_w20/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.inclassexamples_w20;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    TextView temp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        temp = findViewById(R.id.temp);
+        MyHTTPRequest req = new MyHTTPRequest();
+        req.execute("http://api.openweathermap.org/data/2.5/uvi?appid=7e943c97096a9784391a981c4d878b22&lat=45.348945&lon=-75.759389");  //Type 1
+    }
+
+    //Type1     Type2   Type3
+    private class MyHTTPRequest extends AsyncTask<String, Integer, String> {
+        //Type3                Type1
+        public String doInBackground(String... args) {
+            try {
+
+                //create a URL object of what server to contact:
+                URL url = new URL(args[0]);
+
+                //open the connection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                //wait for data:
+                InputStream response = urlConnection.getInputStream();
+
+
+                //JSON reading:
+                //Build the entire string response:
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
+                StringBuilder sb = new StringBuilder();
+
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                String result = sb.toString(); //result is the whole string
+
+
+                // convert string to JSON:
+                JSONObject uvReport = new JSONObject(result);
+
+                //get the double associated with "value"
+                double uvRating = uvReport.getDouble("value");
+
+                Log.i("MainActivity", "The uv is now: " + uvRating);
+                return "The uv is now: " + uvRating;
+
+            } catch (Exception e) {
+                return "Exception Occured: " + e.toString();
+            }
+
+        }
+
+        //Type 2
+        public void onProgressUpdate(Integer... args) {
+
+        }
+
+        //Type3
+        public void onPostExecute(String fromDoInBackground) {
+            temp.setText(fromDoInBackground);
+            Log.i("HTTP", fromDoInBackground);
+        }
+    }
+}
